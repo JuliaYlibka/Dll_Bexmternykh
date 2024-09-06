7c9fe8cd2fa5e4d4d239df47189c9d8d9397214a
Юлия Безматерных <217041@edu.fa.ru>
2024-09-06 10:34:29 +0300

    Добавьте файлы проекта.

---
+++ Dll_Bexmternykh/dllmain.cpp
@@ -0,0 +1,38 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include "windows.h"
+//__declspec(dllexport) void __cdecl TestHello(void);
+BOOL WINAPI DllMain( HINSTANCE hDll,
+                       DWORD  dwReason,
+                       LPVOID lpReserved
+                     )
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, "The DLL is loaded", "DLL Skeleton", MB_OK);
+		break;
+	case DLL_THREAD_ATTACH:
+		MessageBox(NULL, "A thread is created in this process", "DLL Skeleton", MB_OK);
+		break;
+	case DLL_THREAD_DETACH:
+		MessageBox(NULL, "A thread is destroyed in this process", "DLL Skeleton", MB_OK);
+		break;
+	case DLL_PROCESS_DETACH:
+		MessageBox(NULL, "The DLL is unloaded", "DLL Skeleton", MB_OK);
+		break;
+	}
+	return TRUE;
+}
+extern "C"
+extern "C" int count = -5;
+extern "C" int Add(int n)
+{
+	count += n;
+	return count;
+}
+extern "C" int Sub(int n)
+{
+	count -= n;
+	return count;
+}
